a1a26813b021f28fb07f42c5d8fd22404852a345
Giordano Bruno Lazzareschi <giolazzareschi@gmail.com>
2022-05-19 14:36:34 -0300

    Create basic react components

--- gatsby-ts/src/pages/index.tsx
+++ gatsby-ts/src/pages/index.tsx
@@ -1,45 +1,15 @@
 import * as React from "react"
-import { useQuery, gql } from '@apollo/client';
+import RecipeIndex from './Recipes/index';
 
 import './reset.css';
 import './index.scss';
 
-type Post = {
-  id: number;
-  title: string;
-}
-
-type PageData = {
-  posts: {
-    nodes: Array<Post>
-  }
-}
-
 const IndexPage = () => {
-  const { data, loading, error } = useQuery<PageData>(queryPosts);
-
   return (
     <div className="Index">
-      <ul>
-      {
-        data?.posts.nodes.map((post) => {
-          return <li key={post.id}>{ post.title }</li>
-        })
-      }
-      </ul>
+      <RecipeIndex />
     </div>
   )
 }
-
-const queryPosts = gql`
-query allWpPost {
-  posts {
-    nodes {
-      id
-      title
-    }
-  }
-}
-`;
 
 export default IndexPage
